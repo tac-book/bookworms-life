88303be3351d3c2b7974cee40dd4d565a258c022
田窪 <takubo@takubonoMacBook-Air.local>
2020-11-28 06:25:37 +0900

    local test

--- books/local_settings.py
+++ books/local_settings.py
@@ -1,6 +1,8 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SECRET_KEY = 'tdu#qtq8bx^smn8at^6btbaw=sd8!wv25m@ldjy6c7a%7euwpq'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- books/settings.py
+++ books/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tdu#qtq8bx^smn8at^6btbaw=sd8!wv25m@ldjy6c7a%7euwpq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["bookwarms-life.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "bookwarms-life.herokuapp.com"]
 
 
 # Application definition
@@ -140,5 +140,6 @@ except ImportError:
     pass
 
 if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
