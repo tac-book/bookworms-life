4119f05494a80fa54a912794f80387f82aa653a3
田窪 <takubo@takubonoMacBook-Air.local>
2020-11-28 06:29:33 +0900

    allow local debug

--- books/settings.py
+++ books/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tdu#qtq8bx^smn8at^6btbaw=sd8!wv25m@ldjy6c7a%7euwpq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "bookwarms-life.herokuapp.com"]
 
@@ -140,6 +140,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    SECRET_KEY = os.environ['SECRET_KEY']
+    #SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
